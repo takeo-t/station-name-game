77390a7245efdad9de10a4204149c87cc7355e46
takeo-t <takeo-t@118satellite.com>
2025-01-08 00:20:18 +0900

    next.config.tsの修正

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
   output: 'export',
   trailingSlash: true,
-}
+};
 
 export default nextConfig;
